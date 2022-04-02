76be2701ae918b7456c49a27a2c1219ff17f58d1
Alec Harvey <ruinhard@gmail.com>
2022-04-02 02:44:34 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ const Sequelize = require("sequelize")
 require('dotenv').config()
 //create sequelize obj and grab values from environment folder and pass them through. 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    mr6azgrl8keuxivt,
+    uzhtdcblxi2op95r,
+    iy51m0s1tctfmhgm,
     {
         host:"localhost",
         dialect:"mysql",
