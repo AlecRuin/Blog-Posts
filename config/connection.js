d700d1040887eec09f31319e178d16052d9a7236
Alec Harvey <ruinhard@gmail.com>
2022-04-02 03:09:43 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize")
 require('dotenv').config()
 //create sequelize obj and grab values from environment folder and pass them through. 
 const sequelize = new Sequelize(
-    "mr6azgrl8keuxivt",
+    "wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "uzhtdcblxi2op95r",
     "iy51m0s1tctfmhgm",
     {
