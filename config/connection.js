799419f38deb6f38ea364cfd95066596a2115d56
Alec Harvey <ruinhard@gmail.com>
2022-04-02 03:25:01 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 const Sequelize = require("sequelize")
 require('dotenv').config()
 //create sequelize obj and grab values from environment folder and pass them through. 
-const sequelize = new Sequelize(
-    "wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "uzhtdcblxi2op95r",
-    "iy51m0s1tctfmhgm",
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
     {
-        dialect:"mysql"
-    }
-)
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 //finally, export the sequelize obj made. this will be used by the models created.
 module.exports=sequelize
